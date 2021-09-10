2ca3416a5fe1d72f7cd8a41a9432caeb570d06f1
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-09-09 21:13:32 -0300

    feat: Created a limited description with 200 characters

--- src/components/featureMovie/index.js
+++ src/components/featureMovie/index.js
@@ -7,6 +7,12 @@ export default ({ item }) => {
     for (let i in item.genres) {
         genres.push(item.genres[i].name);
     }
+
+    let description = item.overview;
+    if (description.length > 200) {
+        description = description.substring(0, 200) + "...";
+    }
+
     return (
         <section
             className="feature"
@@ -25,7 +31,7 @@ export default ({ item }) => {
                         <div className="feature--seasons">
                             {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? "s" : ""}
                         </div>
-                        <div className="feature--description">{item.overview}</div>
+                        <div className="feature--description">{description}</div>
                         <div className="feature--buttons">
                             <a href={`/watch/${item.id}`} className="watch--button">
                                 ▶ <span className="assistir">Assistir</span>
